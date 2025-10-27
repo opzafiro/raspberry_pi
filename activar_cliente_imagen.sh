5760f673c700042dd2e61d1fe447bcc851652865
zafiro <operacionzafiro271@gmail.com>
2025-10-27 14:23:50 +0000

    se crea el demonio del cliente de imagenes

---
+++ activar_cliente_imagen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#sudo systemctl stop zafiro_imagen.service
+sudo cp services/zafiro_imagen.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable zafiro_imagen.service
+sudo systemctl start zafiro_imagen.service
+sudo systemctl status zafiro_imagen.service
+
+
+
